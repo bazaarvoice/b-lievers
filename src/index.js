12d066c2dc0236e1459f42b78a1d8c203118cca0
JonathanBell09 <jonathan.bell@bazaarvoice.com>
2021-01-30 18:11:54 +0000

    UI Improvements

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
-import BusinessSummary from './BusinessSummary';
-import CountryDropDown from './CountryDropDown';
+import BusinessEntry from './BusinessEntry';
+import CountriesDropdown from './CountriesDropdown';
 
 if (process.env.NODE_ENV !== 'production') {
   const axe = require('react-axe');
@@ -16,24 +16,15 @@ const Home = () => (
       <Header/>
     </header>
     <section className="section1">
-      <CountryDropDown/>
+      <CountriesDropdown/>
     </section>
     <section className="section2">
-      <article>
-        <BusinessSummary/>
-      </article>
-    </section>
-    <footer>
-    </footer>
-  </main>
-);
-
-const BusinessDisplayPage  = () => (
-  <main>
-    <header>
-      <Header/>
-    </header>
-    <section >
+      <BusinessEntry name={"Refill Quarter"} id={"refill-quarter"} url={"https://www.refillquarter.com/"}/>
+      <BusinessEntry name={"387 Ormeau Road"} id={"387-ormeau-road"} url={"https://www.facebook.com/387ormeauroad/"}/>
+      <BusinessEntry name={"5a Café"} id={"5a-cafe"} url={"http://www.generalmerchants.co.uk/stranmillis/"}/>
+      <BusinessEntry name={"Maggie Mays"} id={"maggie-mays"} url={"https://maggiemaysbelfastcafe.co.uk/city-centre-cafe/"}/>
+      <BusinessEntry name={"Sloans Gym"} id={"sloans-gym"} url={"https://www.sloansgymbelfast.com/"}/>
+      <BusinessEntry name={"Ward & Co Fine Foods"} id={"ward-and-co-fine-foods"} url={"https://wardand.co/"}/>
     </section>
     <footer>
     </footer>
