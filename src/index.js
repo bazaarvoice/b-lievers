4c6f3b82e0c4d8679f95b8cf124648f7ec490b1d
JonathanBell09 <jonathan.bell@bazaarvoice.com>
2021-01-30 11:15:26 +0000

    Created Components - getting better at react yayyy

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MenuDropdown from '@bv/aperture-components-react-menu-dropdown';
-import image from './logo.png';
-
+import Header from './Header';
+import BusinessSummary from './BusinessSummary';
+import CountryDropDown from './CountryDropDown';
 
 if (process.env.NODE_ENV !== 'production') {
   const axe = require('react-axe');
   axe(React, ReactDOM, 1000);
 }
 
-const App = () => (
+const Home = () => (
   <main>
     <header>
-      <div className="title_container"><img src={image} className="logo"/><h1 className="title">BV Support Local</h1></div>
+      <Header/>
     </header>
     <section className="section1">
-        <MenuDropdown
-          onChange={(updatedOption, updatedOptions) => {
-            /* ... */
-          }}
-          options={[
-            { value: 'us-texas', label: 'Texas', selected: false },
-            { value: 'india-karnataka', label: 'Karnataka', selected: false },
-            { value: 'northern-ireland', label: 'Northern Ireland', selected: true }
-          ]}
-          searchAriaLabel="search"
-        />
+      <CountryDropDown/>
     </section>
     <section className="section2">
       <article>
-        <div className="review_container">Refill Quarter &nbsp;<div data-bv-show="rating_summary" data-bv-product-id="refill-quarter"></div></div>
+        <BusinessSummary/>
       </article>
+    </section>
+    <footer>
+    </footer>
+  </main>
+);
+
+const BusinessDisplayPage  = () => (
+  <main>
+    <header>
+      <Header/>
+    </header>
+    <section >
     </section>
     <footer>
     </footer>
@@ -40,4 +42,4 @@ const App = () => (
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<Home />, root);
